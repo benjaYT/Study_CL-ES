57f7fe7b47efc51d5d89b28ddf60532c63a4b005
Benja <benja.sepulveda.04@gmail.com>
2022-02-18 18:56:54 -0300

    Primer Backup

---
+++ Programs/All Dev/Python/Calcular IMC/index.py
@@ -0,0 +1,6 @@
+p = float(input())
+a = float(input())
+
+IMC = p / a**2
+
+print("Tu IMC es", IMC)
---
+++ Programs/All Dev/Python/Ejercicios/E.Faciles.py
@@ -0,0 +1,45 @@
+#EJERCICIO 1
+#nombre = str(input())
+
+#print("Ahora estas en la matrix,", nombre)
+
+#EJERCICIO 2
+#n_d = float(input())
+#n_e = int(input())
+#res = n_d + n_e
+
+#print("El resultado de la suma es", res)
+
+#EJERCICIO 3
+
+#n1 = int(input())
+#2 = int(input())
+#res_su = n1 + n2
+
+#print("Suman:", res_su)
+
+#n3 = int(input())
+#res_mul = res_su * n3
+
+#print("Multiplicación de la suma por el último número:", res_mul)
+
+#EJERCICIO 4
+
+k = int(input())
+b_g = float(input())
+c_k = k / b_g
+
+print("El consumo por kilómetro es de", c_k)
+ 
+#EJERCICIO 5
+#EJERCICIO 6
+#EJERCICIO 7
+#EJERCICIO 8
+#EJERCICIO 9
+#EJERCICIO 10
+#EJERCICIO 11
+#EJERCICIO 12
+#EJERCICIO 13
+#EJERCICIO 14
+#EJERCICIO 15
+#EJERCICIO 16
